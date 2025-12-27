9cd350cf954b7ff9ec7d132647a2836eb429f36c
Pranita Zalli <pzalli0204@gmail.com>
2025-12-27 10:59:44 +0530

    Add config example file for API keys

---
+++ config.example.py
@@ -0,0 +1,6 @@
+# config.example.py
+# Rename this file to config.py and add your own API keys
+
+WEATHERSTACK_API_KEY = "YOUR_WEATHERSTACK_API_KEY"
+GEMINI_API_KEY = "YOUR_GEMINI_API_KEY"
+NEWS_API_KEY = "YOUR_NEWSAPI_KEY"
